085084580ae85cee63f5e888f1924b9e01926739
mirthful-concept-6030 <jchristinem@gmail.com>
2020-06-30 19:57:38 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,23 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  !(position_taken?(board, index)) && index.between?(0,8)
+end
+
+def position_taken?(board, index)
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+end
+
+def input_to_index(user_input)
+  user_input = user_input.strip.to_i - 1
+end
